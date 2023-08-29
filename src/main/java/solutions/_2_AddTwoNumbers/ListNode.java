06a956d5c3af02414906fc7abdfa18cddfb2b7ea
SmolSoft <smolsoft@email.cz>
2023-08-29 17:21:24 +0200

    2. Add Two Numbers

---
+++ src/main/java/solutions/_2_AddTwoNumbers/ListNode.java
@@ -0,0 +1,15 @@
+package solutions._2_AddTwoNumbers;
+
+public class ListNode {
+    int      val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {this.val = val;}
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
