837aba4dd8d992f5ba9fd75c80299a07f2b9d638
SmolSoft <smolsoft@email.cz>
2023-09-07 08:33:59 +0200

    541. Reverse String II

--- src/main/java/com/smol/solutions/N541_ReverseStringII.java
+++ src/main/java/com/smol/solutions/N541_ReverseStringII.java
@@ -20,44 +20,26 @@ package com.smol.solutions;
  */
 public class N541_ReverseStringII {
 
-    public String reverseStr01(String s, int k) {
-
-        //todo
-        // - [ ] for every 2k characters counting from the start of the string reverse the first k characters
-        // - [ ] if there are fewer than k characters left, reverse all of them
-        // - [ ] if there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original
-
-        // k=2 ab cd ef ga bc  de fg ab cd ef g
-        // k=2 ba cd fe ga cb  de gf ab dc ef g
-        //     01 23 45 67 89  12 14 16 18 20 21
-
-        //k=3 abc def gab cde fga bcd efg
-        //k=3 cba def bag cde agf bcd gfe
-
-        StringBuilder sb     = new StringBuilder();
-        int           length = s.length();
-
-
-//        int back = k -1;
-//        for (int i = back; i < length; i++) {
-//
-//            for (int left = i; left >= i - back; left--) {
-//                sb.append(s.charAt(left));
-//                System.out.println("reversed: " + sb);
-//            }
-//            //fixme
-//            sb.append(s, i+1, i= Math.min(i + (2 * k)-1,length));
-//            System.out.println("added   : " + sb);
-//    }
-
-
-        return sb.toString();
-    }
-
     /**
-     * credits to: deepVashisth
+     * Runtime 1ms, Beats 87.01%of users with Java <p>
+     * Memory  43.51MB, Beats 38.11%of users with Java <p>
      */
     public String reverseStr(String s, int k) {
+        char[] charArray = s.toCharArray();
+        int    length    = charArray.length;
+        for (int i = 0; i <= length - 1; i += 2 * k) {
+            reverseK(i, Math.min(i + k - 1, length - 1), charArray);
+        }
+        return new String(charArray);
+    }
+
+
+    /**
+     * Runtime 1ms, Beats 87.01%of users with Java <p>
+     * Memory 42.96MB.Beats 98.05%of users with Java <p>
+     * original solution by deepVashisth <p>
+     */
+    public String reverseStr02(String s, int k) {
         char[] str = s.toCharArray();
         int    n   = str.length;
         for (int i = 0; i <= n - 1; i += 2 * k) {
@@ -72,6 +54,7 @@ public class N541_ReverseStringII {
         return ans;
     }
 
+    /** Helper by by by deepVashisth */
     public void reverseK(int i, int j, char[] str) {
         while (i < j) {
             char temp = str[i];
