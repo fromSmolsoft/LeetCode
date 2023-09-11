60cb899f9c5bf82ab6684e42c34193e484bb5773
SmolSoft <smolsoft@email.cz>
2023-09-11 17:08:05 +0200

    141. Linked List Cycle - setup

---
+++ src/main/java/com/smol/solutions/N0141_LinkedListCycle/ListNode.java
@@ -0,0 +1,14 @@
+package com.smol.solutions.N0141_LinkedListCycle;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int      val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/main/java/com/smol/solutions/N0141_LinkedListCycle/N0141_LinkedListCycle.java
@@ -0,0 +1,44 @@
+package com.smol.solutions.N0141_LinkedListCycle;
+
+/**
+ * <h1>141. Linked List Cycle</h1>
+ * Easy<p>
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.<p>
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.<p>
+ * Return true if there is a cycle in the linked list. Otherwise, return false.<p>
+ * <p>
+ * Example 1:<p>
+ * Input: head = [3,2,0,-4], pos = 1<p>
+ * Output: true<p>
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).<p>
+ * <p>
+ * Example 2:<p>
+ * Input: head = [1,2], pos = 0<p>
+ * Output: true<p>
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.<p>
+ * <p>
+ * Example 3:<p>
+ * Input: head = [1], pos = -1<p>
+ * Output: false<p>
+ * Explanation: There is no cycle in the linked list.<p>
+ * <p>
+ * Constraints:<p>
+ * The number of the nodes in the list is in the range [0, 104].<p>
+ * -105 <= Node.val <= 105<p>
+ * pos is -1 or a valid index in the linked-list.<p>
+ * <p>
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?<p>
+ */
+
+public class N0141_LinkedListCycle {
+
+
+    public boolean hasCycle(ListNode head) {
+
+        //todo
+        // iterate over the list until we reach the end
+        // check if there is a cycle in the list that points back to any node withing list
+
+        return false;
+    }
+}
