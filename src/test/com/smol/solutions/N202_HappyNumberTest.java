e9156e393aee78bdda7a3775f3b17d0424dcfb33
SmolSoft <smolsoft@email.cz>
2023-09-12 16:58:24 +0200

    202. Happy Number

---
+++ src/test/com/smol/solutions/N202_HappyNumberTest.java
@@ -0,0 +1,20 @@
+package com.smol.solutions;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class N202_HappyNumberTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "19, true",
+            "2, false",
+            "20000000, false",
+            "99, false"
+    })
+    void isHappy(int n, boolean expected) {
+        assertEquals(expected, new N202_HappyNumber().isHappy(n));
+    }
+}
