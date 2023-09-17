8f956209325ea68b8021b01c9847b43eecc9c833
SmolSoft <smolsoft@email.cz>
2023-09-17 12:52:07 +0200

    21. Merge Two Sorted Lists

--- src/test/com/smol/solutions/TestUtils.java
+++ src/test/com/smol/solutions/TestUtils.java
@@ -7,14 +7,15 @@ public class TestUtils {
 
 
     public int[] convertStringToIntArray(String s, String delimiter) {
+        if (s == null || s.isEmpty()) return new int[]{};
         String[] strings = s.split(delimiter);
         return Arrays.stream(strings).mapToInt(Integer::parseInt).toArray();
     }
 
 
     public String[] convertStringToStringArray(String s, String delimiter) {
-        String[] strings = s.split(delimiter);
-        return strings;
+        if (s == null) return new String[]{};
+        return s.split(delimiter);
     }
 
     public List<String> convertStringToList(String s, String delimiter) {
---
+++ src/test/com/smol/solutions/n0021_merge2sortedlists/N0021_MergeTwoSortedListsTest.java
@@ -0,0 +1,61 @@
+package com.smol.solutions.n0021_merge2sortedlists;
+
+import com.smol.solutions.TestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class N0021_MergeTwoSortedListsTest {
+
+    private static final TestUtils                 utils = new TestUtils();
+    private static final N0021_MergeTwoSortedLists obj   = new N0021_MergeTwoSortedLists();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "'' ,'' ,'' ",
+            "'' , 0,0 ",
+            "1;2;4,     1;3;4,  1;1;2;3;4;4",
+    })
+    void mergeTwoLists(String inp1, String inp2, String exp) {
+
+        ListNode      input1   = covertIntToListNode(inp1);
+        ListNode      input2   = covertIntToListNode(inp2);
+        ListNode      expected = covertIntToListNode(exp);
+        ListNode      actual   = obj.mergeTwoLists(input1, input2);
+        List<Integer> expList  = new ArrayList<>();
+        List<Integer> actList  = new ArrayList<>();
+
+        while (expected != null) {
+            expList.add(expected.val);
+            expected = expected.next;
+        }
+
+        if (actual != null) {
+            while (actual != null) {
+                actList.add(actual.val);
+                actual = actual.next;
+            }
+        }
+
+        Assertions.assertArrayEquals(expList.toArray(), actList.toArray());
+    }
+
+
+    /**
+     * Helper method. Generated ListNode from values within a string
+     */
+    private ListNode covertIntToListNode(String inp1) {
+        ListNode prehead = new ListNode(-1);
+        ListNode tail    = prehead;
+        int[]    values  = utils.convertStringToIntArray(inp1, ";");
+
+        for (int i : values) {
+            tail.next = new ListNode(i);
+            tail = tail.next;
+        }
+        return prehead.next;
+    }
+}
