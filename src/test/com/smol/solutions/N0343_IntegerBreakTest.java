3edb9a0c21db6b4e9b9fb6254a0e1af98ec3e227
SmolSoft <smolsoft@email.cz>
2023-10-06 16:23:18 +0200

    343. Integer Break

---
+++ src/test/com/smol/solutions/N0343_IntegerBreakTest.java
@@ -0,0 +1,25 @@
+package com.smol.solutions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class N0343_IntegerBreakTest {
+
+    N0343_IntegerBreak obj = new N0343_IntegerBreak();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            /*exp       input*/
+            "1,         2",
+            "2,         3",
+            "6,         5",
+            "9,         6",
+            "36,        10",
+            "1549681956,58"
+    })
+    void integerBreak(int exp, int n) {
+        int actual = obj.integerBreak(n);
+        Assertions.assertEquals(exp, actual, "n=" + n);
+    }
+}
