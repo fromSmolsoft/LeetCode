483d7d12655f86dd7702736ec87042d0af499ebb
SmolSoft <smolsoft@email.cz>
2023-12-30 16:05:58 +0100

    TreeFormatter is generic = prints different types of nodes

--- src/test/com/smol/solutions/utils/TreeFormatter.java
+++ src/test/com/smol/solutions/utils/TreeFormatter.java
@@ -48,18 +48,37 @@ public class TreeFormatter {
         return left;
     }
     
-    private List<String> buildLines(TreeNode node) {
+    
+    /**
+     * Root node must include corresponding fields:
+     * <pre>{@code
+     *   public class Node {
+     *      public int val;
+     *      public TreeNode left;
+     *      public TreeNode right;
+     *   }
+     * }</pre>
+     * @param root - root of binary tree
+     * @return String with formatted BinaryTree
+     */
+    public <T> String topDown(T root) {
+        if (root == null) return "";
+        List<String> lines = buildLines(root);
+        return String.join("\n", lines.subList(1, lines.size()));
+    }
+    
+    private <T> List<String> buildLines(T node) {
         if (node == null) return new ArrayList<>();
-        List<String> lines = merge(buildLines(node.left), buildLines(node.right));
-        int half = String.valueOf(node.val).length() / 2;
+        List<String> lines = merge(buildLines(getLeftChild(node)), buildLines(getRightChild(node)));
+        int half = String.valueOf(getNodeValue(node)).length() / 2;
         int i = half;
         if (lines.size() > 0) {
             String line;
             i = lines.get(0).indexOf("*"); // Find index of first subtree
-            if (node.right == null) {
+            if (getRightChild(node) == null) {
                 line = " ".repeat(i) + "┌─┘";
                 i += 2;
-            } else if (node.left == null) {
+            } else if (getLeftChild(node) == null) {
                 line = " ".repeat(i = indent(lines, i - 2)) + "└─┐";
             } else {
                 int dist = lines.get(0).length() - 1 - i; // Find distance between subtree roots
@@ -68,17 +87,69 @@ public class TreeFormatter {
             }
             lines.set(0, line);
         }
-        lines.add(0, " ".repeat(indent(lines, i - half)) + node.val);
+        lines.add(0, " ".repeat(indent(lines, i - half)) + getNodeValue(node));
         lines.add(0, " ".repeat(i + Math.max(0, half - i)) + "*"); // Add a marker for caller
         return lines;
     }
     
-    /**
-     * @return String with formatted BinaryTree
-     */
-    public String topDown(TreeNode root) {
-        if (root == null) return "";
-        List<String> lines = buildLines(root);
-        return String.join("\n", lines.subList(1, lines.size()));
+    private static <T> int getNodeValue(T node) {
+        try {
+            return (int) node.getClass().getField("val").get(node);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
+    
+    private static <T> T getLeftChild(T node) {
+        try {
+            return (T) node.getClass().getField("left").get(node);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private static <T> T getRightChild(T node) {
+        try {
+            return (T) node.getClass().getField("right").get(node);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    
+    // None generic version
+//    private List<String> buildLines(TreeNode node) {
+//        if (node == null) return new ArrayList<>();
+//        List<String> lines = merge(buildLines(node.left), buildLines(node.right));
+//        int half = String.valueOf(node.val).length() / 2;
+//        int i = half;
+//        if (lines.size() > 0) {
+//            String line;
+//            i = lines.get(0).indexOf("*"); // Find index of first subtree
+//            if (node.right == null) {
+//                line = " ".repeat(i) + "┌─┘";
+//                i += 2;
+//            } else if (node.left == null) {
+//                line = " ".repeat(i = indent(lines, i - 2)) + "└─┐";
+//            } else {
+//                int dist = lines.get(0).length() - 1 - i; // Find distance between subtree roots
+//                line = String.format("%s┌%s┴%s┐", " ".repeat(i), "─".repeat(dist / 2 - 1), "─".repeat((dist - 1) / 2));
+//                i += dist / 2;
+//            }
+//            lines.set(0, line);
+//        }
+//        lines.add(0, " ".repeat(indent(lines, i - half)) + node.val);
+//        lines.add(0, " ".repeat(i + Math.max(0, half - i)) + "*"); // Add a marker for caller
+//        return lines;
+//    }
+//
+//    /**
+//     * @return String with formatted BinaryTree
+//     */
+//    public String topDown(TreeNode root) {
+//        if (root == null) return "";
+//        List<String> lines = buildLines(root);
+//        return String.join("\n", lines.subList(1, lines.size()));
+//    }
+//
 }
