e7f16c5fab9787a127770db7b2573f0bd0f637ad
SmolSoft <smolsoft@email.cz>
2024-01-02 11:23:16 +0100

    116. Populating Next Right Pointers in Each Node

--- src/main/java/com/smol/solutions/N0116_PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/smol/solutions/N0116_PopulatingNextRightPointersInEachNode.java
@@ -2,6 +2,9 @@ package com.smol.solutions;
 
 import com.smol.solutions.utils.Node;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * <h1>116. Populating Next Right Pointers in Each Node</h1>
  * Medium, #Linked List, #Tree, #Depth-FirstSearch, #Breadth-FirstSearch, #BinaryTree
@@ -47,11 +50,99 @@ import com.smol.solutions.utils.Node;
 public class N0116_PopulatingNextRightPointersInEachNode {
     
     /**
-     *
+     * <h2>Iterative solution</h2>
+     * <pre>
+     * Runtime 3ms Beats    16.15%of users with Java
+     * Memory 44.08MB Beats 23.38%of users with Java
+     * </pre>
+     */
+    public Node connectI01(Node root) {
+        if (root == null) return null;
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            Node right = null;
+            int levelWidth = queue.size();
+            
+            for (int i = 0; i < levelWidth; i++) {
+                Node curr = queue.poll();
+                
+                if (right != null) right.next = curr;
+                right = curr;
+                
+                if (curr == null) continue;
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+            }
+            
+        }
+        return root;
+    }
+    
+    
+    /**
+     * <h2>Iterative, without stack or queue</h2>
+     * <pre>
+     * Runtime 1ms  Beats 72.48 % of users with Java
+     * Memory 44.10MB Beats 23.38 % of users with Java
+     * </pre>
+     */
+    public Node connectI02(Node root) {
+        
+        Node head = root;
+        Node nextHead = new Node(0);
+        Node nextTail;
+        
+        while (head != null) {
+            (nextTail = nextHead).next = null;
+            
+            while (head != null) {
+                
+                if (head.left != null) {
+                    nextTail.next = head.left;
+                    nextTail = head.left;
+                }
+                
+                if (head.right != null) {
+                    nextTail.next = head.right;
+                    nextTail = head.right;
+                }
+                head = head.next;
+            }
+            head = nextHead.next;
+        }
+        return root;
+    }
+    
+    /**
+     * <h2>Recursion</h2>
+     * Works only for balanced tree with all children non-null.
+     * <pre>{@code
+     *  Values = {1,2,3,4,5,null,7}
+     *       Initial step (after root)     |          Next step
+     *               1                     |               1
+     *        ┌──────┴───────┐             |        ┌──────┴───────┐
+     * root = 2 -----------> 3 = root.next | root = 2 -----------> 3 = root.next
+     *   ┌────┴───┐     ┌────┴─────┐       |   ┌────┴───┐     ┌────┴─────┐
+     *   4 -----> 5     6          7       |   4        5 --> 6          7
+     *   ↑        ↑                        |            ↑     ↑
+     * left     right                      |         right  root.next.left
+     * }</pre>
      */
     public Node connect(Node root) {
-        //todo
-        return new Node();
+        if (root == null) return null;
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+        if (root.right != null && root.next != null) {
+            if (root.next.left != null) root.right.next = root.next.left;
+            else root.right.next = root.next.right;
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
     }
     
     
---
+++ src/test/com/smol/solutions/N0116_PopulatingNextRightPointersInEachNodeTest.java
@@ -0,0 +1,81 @@
+package com.smol.solutions;
+
+import com.smol.solutions.utils.Node;
+import com.smol.solutions.utils.TUtils;
+import com.smol.solutions.utils.TreeFormatter;
+import com.smol.solutions.utils.Trees;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+class N0116_PopulatingNextRightPointersInEachNodeTest {
+    
+    private final N0116_PopulatingNextRightPointersInEachNode obj = new N0116_PopulatingNextRightPointersInEachNode();
+    private final List<Method> methods = TUtils.reflectMethods(obj.getClass(), "connect");
+    private final TreeFormatter treeFormatter = new TreeFormatter();
+    
+    /**
+     * <pre> {@code
+     * struct Node {
+     *   int val;
+     *   Node *left;
+     *   Node *right;
+     *   Node *next;
+     * }
+     * }</pre>
+     * <p>
+     *
+     * <pre> {@code
+     * Example 1:
+     *         1                 1 --------> null
+     *       /   \             /   \
+     *      2     3           2---->3 -----> null
+     *    / \     / \       / \     / \
+     *   4   5   6   7     4-->5-->6-->7 --> null
+     * Input: root = [1,2,3,4,5,6,7]
+     * Output: [1,#,2,3,#,4,5,6,7,#]
+     * }</pre>
+     * Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+     * <p> <pre> {@code
+     *
+     * Example 2:
+     * Input: root = []
+     * Output: []
+     *
+     * Constraints:
+     * -> The number of nodes in the tree is in the range [0, 212 - 1].
+     * -> -1000 <= Node.val <= 1000
+     *
+     * }</pre>
+     */
+    @ParameterizedTest
+    @CsvSource(delimiter = ';', value = {
+            "1,#,2,3,#,4,5,6,7,#; 1,2,3,4,5,6,7",
+            " ; ",
+    })
+    void connect(String sExp, String sRoot) {
+        sExp = TUtils.removeSubStrings(sExp, ",#");
+        Node exp = Trees.buildBiTreeWithNext(TUtils.StringToIntegerArray(sExp, ","), Node.class);
+        Node root = Trees.buildBiTree(TUtils.StringToIntegerArray(sRoot, ","), Node.class); //Input `root` gets modified by tested methods
+        String expMessage = treeFormatter.topDownConnected(exp);
+        
+        methods.forEach(m -> {
+            Node act = null;
+            try {
+                act = (Node) m.invoke(obj, root);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            
+            String actMessage = treeFormatter.topDownConnected(act);
+            Assertions.assertEquals(expMessage, actMessage,
+                    "\nmethod: " + m.getName() +
+                    "\ninp:" + sRoot);
+        });
+    }
+    
+}
